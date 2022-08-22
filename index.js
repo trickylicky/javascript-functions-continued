c2add9988a1bdc03d941614ac7010f5386c88209
Tricky Licky <wangombevictor5@gmail.com>
2022-08-22 08:19:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+function saturdayFun(activity1 = "roller-skate"){
+    
+    return `This Saturday, I want to ${activity1}!`
+}
+
+function mondayWork(activity2= "go to the office"){
+    return `This Monday, I will ${activity2}.`
+}
+
+
+// function wrapAdjective(wrapAdjective="*"){
+//     function 
+// }
+
+// function wrapAdjective(activity = '||') {
+//     function prami(pram = 'a dedicated programmer'){
+//         return pram;
+//     }
+//     return `You are ${activity}${prami()}${activity}!`
+// } 
+const wrapAdjective = (activity = '*') => {
+    return (pram = 'special') => {
+        return `You are ${activity}${pram}${activity}!`
+    }
+} 
